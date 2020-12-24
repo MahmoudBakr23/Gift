0124992a0c8db6a6aea82abe550fa5d9369eea76
Mahmoud Bakr <mbakr6821@gmail.com>
2020-12-24 17:02:40 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,44 @@
+gsap.to('#moon', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 1.5,
+})
+gsap.to('#bg', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 1.1,
+})
+gsap.to('#gift', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 1.5,
+    y: -500,
+    x: 2200
+})
+gsap.to('#tree', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    x: -500
+})
+gsap.to('#cloud1', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    x: -200
+})
+gsap.to('#cloud2', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    x: 300
+})
+gsap.to('#text', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    y: -800
+})
